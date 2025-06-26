15719ae4b0e2ee9804498d59a79faeda2dd4e537
JasonNicholas07 <jasonOey07@gmail.com>
2025-06-26 14:06:41 +0700

    Create productOfTwoInt.c

---
+++ Basic Declaration/productOfTwoInt.c
@@ -0,0 +1,19 @@
+//calculate product of two integers
+
+#include <stdio.h>
+
+int main () {
+	int x, y;
+	
+	printf("Input the first integer: ");
+	scanf("%d", &x);
+	
+	printf("Input the second integer: ");
+	scanf("%d", &y);
+	
+	printf("\nProduct of the above two integers = %d\n", x*y);
+	
+	
+	
+	return 0;
+}
