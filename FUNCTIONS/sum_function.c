f56be79104c2a1ef408d6eb94b74893e6aa7aaf7
JasonNicholas07 <jasonOey07@gmail.com>
2025-06-26 14:30:55 +0700

    Update sum_function.c

--- FUNCTIONS/sum_function.c
+++ FUNCTIONS/sum_function.c
@@ -6,14 +6,15 @@ int sum (int x, int y);				//sum func declaration
 
 int main (){
 	int num1, num2, result;				//variable
-	
+
+	printf("SUM FUNCTION\n");
 	printf("Input first number: ");		//read input
 	scanf("%d", &num1);
 	printf("Input second number: ");
 	scanf("%d", &num2);
 	
 	result = sum(num1, num2);			//call sum function
-	printf("Result : %d\n", result);	//output
+	printf("\nResult : %d\n", result);	//output
 		
 	return 0;
 }
